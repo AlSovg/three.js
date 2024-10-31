222e5bea2ff056bbdcc7a42d97c04adcf6ff32c8
AlSovg <141185449+AlSovg@users.noreply.github.com>
2024-10-31 22:19:15 +0300

    Изучение функций камеры, использование функций с использованием времени

--- src/index.js
+++ src/index.js
@@ -3,48 +3,15 @@ import './main.scss';
 
 const scene = new THREE.Scene();
 
-const quaternion = new THREE.Quaternion();
-
 const AxesHelper = new THREE.AxesHelper(5)
 
 scene.add(AxesHelper)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "purple", wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
 
-const group = new THREE.Group();
-group.scale.z = 2
-group.rotation.y = Math.PI / 4;
-
-const cube1 = new THREE.Mesh(geometry, material);
-cube1.position.set(-2, 0, 0);
-
-const cube2 = new THREE.Mesh(geometry, material);
-cube2.position.set(0, 0, 0);
-
-const cube3 = new THREE.Mesh(geometry, material);
-cube3.position.set(2, 0, 0);
-
-group.add(cube1)
-group.add(cube2)
-group.add(cube3)
-
-
-
-scene.add(group)
-//
-// cube.position.x = -3
-// cube.position.y = -3
-// cube.position.z = 1
-//
-// cube.scale.set(1,2,0.5)
-//
-// cube.rotation.x = Math.PI / 4
-// cube.rotation.y = Math.PI / 4
-//
-// cube.rotation.reorder('YXZ')
-
-// scene.add(cube);
+scene.add(cube);
 
 const size = {
   width: 600,
@@ -52,7 +19,7 @@ const size = {
 };
 
 const camera = new THREE.PerspectiveCamera(75,size.width / size.height, 0.1, 1000 );
-camera.position.set(0, 1, 10);
+camera.position.set(0, 0, 3);
 
 scene.add(camera)
 
@@ -61,3 +28,23 @@ const renderElement = document.querySelector(".canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: renderElement });
 renderer.setSize(size.width, size.height);
 renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+// let time = Date.now()
+
+const tick = () => {
+  // const currentTime = Date.now();
+  // const delta = currentTime - time;
+  // time = currentTime;
+  const elapsedTime = clock.getElapsedTime();
+
+  camera.position.x = Math.cos(elapsedTime)
+  camera.position.y = Math.sin(elapsedTime)
+  camera.lookAt(cube.position)
+
+  // cube.rotation.y += 0.01 * elapsedTime;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+tick();
